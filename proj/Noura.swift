0abe7d7131ff7b0262cc0f3db436a9ff3ddc628b
Norah9999 <norah.m4432@gmail.com>
2025-09-29 17:18:09 +0300

    my file

---
+++ proj/Noura.swift
@@ -0,0 +1,18 @@
+//
+//  Noura.swift
+//  proj
+//
+//  Created by NORAH on 07/04/1447 AH.
+//
+
+import SwiftUI
+
+struct Noura: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Noura()
+}
